677d812f8b201e9948f043d5822867bbabbb8bcc
ncb6206 <ncb6206@gmail.com>
2023-10-04 03:15:14 +0900

    Feat : 채널명 수정

---
+++ components/modals/delete-channel-modal.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { useState } from 'react';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+
+import qs from 'query-string';
+import { useModal } from '@/hooks/use-modal-store';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+export const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === 'deleteChannel';
+  const { server, channel } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      await axios.delete(url);
+
+      onClose();
+      router.refresh();
+      router.push(`/servers/${server?.id}`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            채널 삭제
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            정말로{' '}
+            <span className="font-semibold text-indigo-500">
+              #{channel?.name}
+            </span>{' '}
+            채널을 삭제시키실 겁니까?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full ">
+            <Button
+              disabled={isLoading}
+              onClick={onClick}
+              variant="primary"
+              className="py-4 px-9 font-semibold"
+            >
+              네
+            </Button>
+            <Button
+              disabled={isLoading}
+              onClick={onClose}
+              variant="ghost"
+              className="py-4 px-6 font-semibold"
+            >
+              아니요
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
