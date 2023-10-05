4379cc64a948b8a6934489caf299d04f6dbe7d33
ncb6206 <ncb6206@gmail.com>
2023-10-05 16:53:10 +0900

    Feat : 채팅 입력 component 설정

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,10 @@
-import { redirectToSignIn } from '@clerk/nextjs';
-
-import { currentProfile } from '@/lib/current-profile';
-import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
+
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { currentProfile } from '@/lib/current-profile';
 import { ChatHeader } from '@/components/chat/chat-header';
+import { ChatInput } from '@/components/chat/chat-input';
 
 interface ChannelIdPageProps {
   params: {
@@ -42,6 +43,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
       />
     </div>
   );
